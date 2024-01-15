da15b0051b5e81612130efcf003491da165574d5
Tomoi96 <tomislav.lazaret@gmail.com>
2024-01-15 20:53:10 +0100

    added first variables, comments, '<script> moved to </body>

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,21 @@
-var camperbot;
-var xp;
+//adding variables and their values. 'let' variables can be reassigned.
+let camperbot;
+let xp = 0;
+let health = 100;
+let gold = 50;
+let currentWeapon = 0;
+let fighting;
+let monsterHealth;
+let inventory = ["stick"];
+//variable that is not going to be reassigned. If you are not going to assign a new value to a variable, 
+//it is best practice to use the 'const' keyword to declare it instead of the 'let' keyword.
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
+const text = document.querySelector("#text");
+const xpText = document.querySelector("#xptext");
+const healthText = document.querySelector("#healthText");
+const goldText = document.querySelector("#goldText");
+const monsterStats = document.querySelector("#monsterStats");
+const monsterName = document.querySelector("#monsterName");
+const monsterHealthText = document.querySelector("#monsterHealth")
